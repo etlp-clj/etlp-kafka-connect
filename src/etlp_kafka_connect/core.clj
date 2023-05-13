121991e87ee889f6d67a6156e2bad0246fe7f422
Rahul Gaur <rahul.nbg@gmail.com>
2023-05-13 08:42:35 +0530

    init: base for kafka connect and kstream processor

---
+++ src/etlp_kafka_connect/core.clj
@@ -0,0 +1,6 @@
+(ns etlp-kafka-connect.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/etlp_kafka_connect/core_test.clj
@@ -0,0 +1,7 @@
+(ns etlp-kafka-connect.core-test
+  (:require [clojure.test :refer :all]
+            [etlp-kafka-connect.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
